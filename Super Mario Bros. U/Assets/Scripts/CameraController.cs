1442f56a0e68dc9a6d847a380610ab60baaeb293
Reynir Aron Magnússon <reyniraron@gmail.com>
2019-05-03 08:47:01 +0000

    Add camera controller

---
+++ Super Mario Bros. U/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Transform levelEnd;
+    private Camera camera;
+    private Vector3 offset;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        offset = transform.position - player.transform.position;  // Fjarlægð frá leikmanni
+    }
+
+    void LateUpdate()
+    {
+        // Færa myndavél ef leikmaður er á miðjum skjánum og myndavélin er ekki farin fram hjá enda borðsins
+        if (camera.WorldToScreenPoint(player.transform.position).x > Screen.width / 2 &&
+            camera.WorldToScreenPoint(levelEnd.transform.position).x > Screen.width)
+        {
+            Vector3 newPos = player.transform.position + offset;
+            newPos.y = transform.position.y;  // y-staðsetningin á alltaf að vera sú sama
+            transform.position = newPos;
+        }
+    }
+}
